7e0ec78660fcc057bfef48966a3897141fb26ef9
Hoahumg <lethihoa18032001@gamil.com>
2023-03-20 14:39:02 +0700

    BTVN0703

--- BaiThucHanh0703/Controllers/HomeController.cs
+++ BaiThucHanh0703/Controllers/HomeController.cs
@@ -1,17 +1,42 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using BaiThucHanh0703.Models;
+using BaiThucHanh0703.Models.Process;
 
 namespace BaiThucHanh0703.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    GiaiPhuongTrinh GPT = new GiaiPhuongTrinh();
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
     }
+    public IActionResult GBTB1()
+    {
+        return View();
+    }
+    [HttpPost]
+
+     public IActionResult GBTB1(string hesoA, string hesoB)
+    {
+        double a = 0,b = 0;
+        string thongbao = GPT.GiaiPhuongTrinhB1(a , b);
+        if(!string.IsNullOrEmpty(hesoA)) a = Convert.ToDouble(hesoA);
+        if(!string.IsNullOrEmpty(hesoB)) b = Convert.ToDouble(hesoB);
+        if(a == 0 ){
+            thongbao = "Khong phai phuong trinh bac nhat";
+        }
+        else{
+            thongbao = GPT.GiaiPhuongTrinhB1(a,b);
+        }
+        ViewBag.message = thongbao;
+
+        return View();
+    }
+    
 
     public IActionResult Index()
     {
---
+++ BaiThucHanh0703/Models/Process/GiaiPhuongTrinh.cs
@@ -0,0 +1,14 @@
+namespace BaiThucHanh0703.Models.Process
+{
+    public class GiaiPhuongTrinh
+    {
+        //xay dung cac phuong thuc
+        //phuong thuc giai phuong trinh bac 1
+        public string GiaiPhuongTrinhB1(double a, double b)
+        {
+            double x = -b/a;
+           //code giai phuong trinh
+           return "Nghiem cua phuong trinh x = " + x;
+        }
+    }
+}
